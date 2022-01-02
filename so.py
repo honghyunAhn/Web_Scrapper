bb9a1b9fb1ac3a0f5899d693a3bb44407552960e
honghyunAhn <anh2073@naver.com>
2022-01-02 22:55:02 +0900

    Web Scrapper finish

--- so.py
+++ so.py
@@ -30,6 +30,7 @@ def extract_job(html):
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping S0: Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
